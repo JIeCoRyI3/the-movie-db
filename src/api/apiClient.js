8a9795123690e8ea0c9e826833e2fd1704d1f1fc
JIeCoRyI3 <jiecoryi3@gmail.com>
2020-12-11 21:33:41 +0200

    Testing

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -7,17 +7,23 @@ export class ApiClient {
         return instance;
     }
 
-    postGlobalResource = async (url, data = null, headers, method, jsonNeed = false) => {
-        const res = await fetch(`${this.apiBase}${url}`,
-            {
-                method,
-                body: data ? JSON.stringify(data) : null,
-                headers: headers || null,
-            });
+    postGlobalResource = async (
+        url,
+        data = null,
+        headers,
+        method,
+        jsonNeed = false
+    ) => {
+        const res = await fetch(`${this.apiBase}${url}`, {
+            method,
+            body: data ? JSON.stringify(data) : null,
+            headers: headers || null,
+        });
 
         if (!res.ok) {
-            throw new Error(`Could not fetch ${url}`
-                + `, received ${res.status}`);
+            throw new Error(
+                `Could not fetch ${url}` + `, received ${res.status}`
+            );
         }
         if (jsonNeed) {
             return await res.json();
@@ -27,19 +33,47 @@ export class ApiClient {
 
     createFilterString(obj) {
         let filterString = `?api_key=${this.apiKey}&`;
-        for(const [key, value] of Object.entries(obj)) {
+        for (const [key, value] of Object.entries(obj)) {
             filterString += `${key}=${value}&`;
         }
         return filterString.slice(0, -1);
     }
 
     //  filter
-    filterBy = async (filterObj) => await this.postGlobalResource( `/search/movie` + this.createFilterString(filterObj), null, null, 'GET', true);
+    filterBy = async (filterObj) =>
+        await this.postGlobalResource(
+            '/search/movie' + this.createFilterString(filterObj),
+            null,
+            null,
+            'GET',
+            true
+        );
 
     //  details
-    detailsFromFilm = async (id) => await this.postGlobalResource(`/movie/${id}` + this.createFilterString(), null, null, 'GET', true);
+    detailsFromFilm = async (id) =>
+        await this.postGlobalResource(
+            `/movie/${id}` + this.createFilterString(),
+            null,
+            null,
+            'GET',
+            true
+        );
 
     //  genres
-    getAllGenres = async () => await this.postGlobalResource(`/genre/movie/list` + this.createFilterString(), null, null, 'GET', true);
-    filterWithGenres = async (genreObj) => await this.postGlobalResource(`/discover/movie/` + this.createFilterString(genreObj), null, null, 'GET', true)
+    getAllGenres = async () =>
+        await this.postGlobalResource(
+            `/genre/movie/list` + this.createFilterString(),
+            null,
+            null,
+            'GET',
+            true
+        );
+    filterWithGenres = async (genreObj) =>
+        await this.postGlobalResource(
+            `/discover/movie/` + this.createFilterString(genreObj),
+            null,
+            null,
+            'GET',
+            true
+        );
 }
