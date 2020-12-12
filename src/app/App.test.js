67551a597a9b35200c92357a078add3cb6cfa525
JIeCoRyI3 <jiecoryi3@gmail.com>
2020-12-12 16:10:12 +0200

    Add Jest and Enzyme

---
+++ src/app/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+describe('App', () => {
+    it('should something', () => {
+        const component = shallow(<App />);
+        expect(component.find('p')).toHaveLength(1);
+    });
+});
