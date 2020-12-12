f40e4825ef4b7a8dd18415337929804f7f082950
Maksym Kozlov <54378060+SkiF-ex@users.noreply.github.com>
2020-12-12 14:44:47 +0200

    fixed code, styles and added logo image

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import './footer.css';
+import { footer, footer_logo } from  './Footer.module.css';
+import logo from '../../assets/images/logo.png';
 
 export default function Footer () {
     return (
-        <footer className="footer">
-            <h3 className="footer_logo">TheMovieDb</h3>
+        <footer className={ footer }>
+            <img className={footer_logo} src={logo} alt="logo"></img>
         </footer>
     );
 };
