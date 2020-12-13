342178c440a8ee12c3fddb3f72078e3357738fae
Anton Burchak <anton.burchak@epam.com>
2020-12-13 12:06:50 +0200

    Inject other components into App component

--- src/app/App.js
+++ src/app/App.js
@@ -1,16 +1,21 @@
-import styles from './App.module.css';
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MainContainer from '../components/MainContainer';
+import Header from '../components/Header';
+import styles from './App.module.css';
 
 function App() {
     return (
-        <div className={styles.App}>
-            <MainContainer>
-                <div>1</div>
-                <div>2</div>
-                <div>3</div>
-            </MainContainer>
-        </div>
+        <BrowserRouter>
+            <div className={styles.App}>
+                <Header />
+                <MainContainer>
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                </MainContainer>
+            </div>
+        </BrowserRouter>
     );
 }
 
