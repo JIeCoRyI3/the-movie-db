64bdc672ae541c2a255699ea070ce66c3419e2e1
Alexey Savchenko <wasxela@gmail.com>
2020-12-13 14:20:39 +0200

    delete comments, use template strings instead concatenation

--- src/components/FilmCardComponent/FilmCard.jsx
+++ src/components/FilmCardComponent/FilmCard.jsx
@@ -3,28 +3,23 @@ import PropTypes from 'prop-types';
 import styles from './FilmCard.module.css';
 
 const FilmCard = () => {
-    // const { poster_path, title, release_date, genres } = props;
     return (
-        <div className={' card ' + styles.filmCard} style={{width: 18 + 'rem'}}>
+        <div className={`card ${styles.filmCard}`} style={{width: 18 + 'rem'}}>
             <img
                 src={'poster_path'}
-                className={' card-img-top ' + styles.filmCardImg}
+                className={`card-img-top ${styles.filmCardImg}`}
                 alt="poster"
             />
-            <div className={' card-body ' + styles.filmCardBody}>
-                <h5 className={' card-title ' + styles.filmCardTitle}>{'title'}</h5>
-                <p className={' card-text ' + styles.filmCardYear}>{'release_date'}</p>
-                <p className={' card-text ' + styles.filmCardGenresList}>
-                    {/*{genres.map((item) => (*/}
-                    {/*    <span key={item} className={' card-text ' + styles.filmCardGenres}>*/}
-                    {/*        {item}*/}
-                    {/*    </span>*/}
-                    {/*))}*/}
+            <div className={`card-body ${styles.filmCardBody}`}>
+                <h5 className={`card-title ${styles.filmCardTitle}`}>{'title'}</h5>
+                <p className={`card-text ${styles.filmCardYear}`}>{'release_date'}</p>
+                <p className={`card-text ${styles.filmCardGenresList}`}>
                 </p>
             </div>
         </div>
     );
 };
+
 
 FilmCard.propTypes = {
     poster_path: PropTypes.string.isRequired,
