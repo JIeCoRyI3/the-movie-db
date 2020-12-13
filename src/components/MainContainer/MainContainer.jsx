d82d961f546cac3046b7246e7bb2591da94a0625
Anton Burchak <anton.burchak@epam.com>
2020-12-13 17:29:59 +0200

    Rebase main container

--- src/components/MainContainer/MainContainer.jsx
+++ src/components/MainContainer/MainContainer.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './MainContainer.module.css';
+import FilmsContainer from '../FilmsContainer/';
+// import NotFound from '../NotFound';
 
 function MainContainer(props) {
-    return <main className={styles.mainContainer}>{props.children}</main>;
+    return (
+        <main className={styles.mainContainer}>
+            {props.films ? <FilmsContainer films={props.films} /> : null}
+        </main>
+    );
 }
 
 MainContainer.propTypes = {
-    children: PropTypes.oneOfType([
-        PropTypes.arrayOf(PropTypes.node),
-        PropTypes.node,
-    ]),
+    // films: PropTypes.oneOfType([
+    //     PropTypes.arrayOf(PropTypes.object),
+    //     PropTypes.node,
+    // ]),
+    films: PropTypes.arrayOf(PropTypes.object),
 };
 
 export { MainContainer };
