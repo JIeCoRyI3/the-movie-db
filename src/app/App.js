dcf7b4984be6d257711bb4dc58814df79652e42c
Maksym Kozlov <54378060+SkiF-ex@users.noreply.github.com>
2020-12-13 13:48:51 +0200

    Fixed code style

--- src/app/App.js
+++ src/app/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import styles from './App.module.css';
 import MainContainer from '../components/MainContainer';
 import Footer from '../components/Footer';
+import CurrentFilmDescription from '../components/CurrentFilm';
 
 function App() {
     return (
         <>
+            <CurrentFilmDescription />
             <div className={styles.App}>
                 <MainContainer>
                     <div>1</div>
