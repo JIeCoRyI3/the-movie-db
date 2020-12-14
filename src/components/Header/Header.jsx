22cb36b2e61013409773354ce2fa3de7a152dc1b
Maksym Kozlov <54378060+SkiF-ex@users.noreply.github.com>
2020-12-14 20:18:09 +0200

    Fixed code style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,41 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Logo from '../Logo';
 import styles from './Header.module.css';
 
-function Header(props) {
-    console.log(props);
-    return (
-        <section className={styles.headerComponent}>
-            <header className={styles.header}>
-                <Link to="/" className={`btn btn-primary ${styles.logoHeader}`}>
-                    <Logo />
-                </Link>
-                <Route
-                    path="/film/:id"
-                    render={() => (
-                        <Link
-                            to="/"
-                            className={`btn btn-primary ${styles.goBack}`}
-                        >
-                            go back
-                        </Link>
-                    )}
-                />
-            </header>
-            <div className={styles.headerAdditional}>{props.children}</div>
-            <div className={styles.headerSortBlock}>
-                <p className={styles.sortBlockTitle}>Sort by: </p>
-                <button className={`btn btn-primary ${styles.sortRating}`}>
-                    rating
-                </button>
-                <button className={`btn btn-primary ${styles.sortRelease}`}>
-                    release date
-                </button>
-            </div>
-        </section>
-    );
+class Header extends Component {
+    render() {
+        console.log(this.props);
+        return (
+            <section className={styles.headerComponent}>
+                <header className={styles.header}>
+                    <Link
+                        to="/"
+                        className={`btn btn-primary ${styles.logoHeader}`}
+                    >
+                        <Logo />
+                    </Link>
+                    <Route
+                        path="/film/:id"
+                        render={() => (
+                            <Link
+                                to="/"
+                                className={`btn btn-primary ${styles.goBack}`}
+                            >
+                                go back
+                            </Link>
+                        )}
+                    />
+                </header>
+                <div className={styles.headerAdditional}>
+                    {this.props.children}
+                </div>
+                <div className={styles.headerSortBlock}>
+                    <p className={styles.sortBlockTitle}>Sort by: </p>
+                    <button className={`btn btn-primary ${styles.sortRating}`}>
+                        rating
+                    </button>
+                    <button className={`btn btn-primary ${styles.sortRelease}`}>
+                        release date
+                    </button>
+                </div>
+            </section>
+        );
+    }
 }
 Header.propTypes = {
     children: PropTypes.oneOfType([
