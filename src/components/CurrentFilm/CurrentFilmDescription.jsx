5b5435d5d982d381b286a2c609d5510cecb59423
Maksym Kozlov <54378060+SkiF-ex@users.noreply.github.com>
2020-12-14 17:15:43 +0200

    Fixed code style

--- src/components/CurrentFilm/CurrentFilmDescription.jsx
+++ src/components/CurrentFilm/CurrentFilmDescription.jsx
@@ -1,32 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
 import styles from './CurrentFilmDescription.module.css';
 
-export default function CurrentFilmDescription() {
-    return (
-        <div className={styles.currentFilmContainer}>
-            <img
-                className={styles.poster}
-                src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/sWgBv7LV2PRoQgkxwlibdGXKz1S.jpg"
-                alt="The Mandalorian"
-                title="The Mandalorian"
-            />
-            <div className={styles.detailsContainer}>
-                <div className={styles.TitleContainer}>
-                    <h4 className={styles.TitleContainerFilmName}>
-                        The Mandalorian
-                    </h4>
-                    <h4>8.5</h4>
-                </div>
-                <div>
-                    <h5>2019</h5>
-                    <h5>
-                        After the fall of the Galactic Empire, lawlessness has
-                        spread throughout the galaxy. A lone gunfighter makes
-                        his way through the outer reaches, earning his keep as a
-                        bounty hunter.
-                    </h5>
+class CurrentFilmDescription extends Component {
+    render() {
+        console.log(this.props.match.params.id);
+        return (
+            <div className={styles.currentFilmContainer}>
+                <img
+                    className={styles.poster}
+                    src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/sWgBv7LV2PRoQgkxwlibdGXKz1S.jpg"
+                    alt="The Mandalorian"
+                    title="The Mandalorian"
+                />
+                <div className={styles.detailsContainer}>
+                    <div className={styles.TitleContainer}>
+                        <h4 className={styles.TitleContainerFilmName}>
+                            The Mandalorian
+                        </h4>
+                        <h4>8.5</h4>
+                    </div>
+                    <div>
+                        <h5>2019</h5>
+                        <h5>
+                            After the fall of the Galactic Empire, lawlessness
+                            has spread throughout the galaxy. A lone gunfighter
+                            makes his way through the outer reaches, earning his
+                            keep as a bounty hunter.
+                        </h5>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
+
+export default withRouter(CurrentFilmDescription);
